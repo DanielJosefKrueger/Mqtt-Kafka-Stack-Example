822e8e2fb72a0dc393a647e28ba397a1eb15f8bd
Daniel Krueger <you@example.com>
2021-09-28 23:00:58 +0200

    CONSUMER: improve print

--- DataAnalyse/docker/consume.py
+++ DataAnalyse/docker/consume.py
@@ -3,9 +3,9 @@ from kafka import KafkaConsumer
 if __name__ == '__main__':
     # Kafka Consumer
     consumer = KafkaConsumer(
-        'messages',
-        bootstrap_servers='localhost:9092',
+        'kafka-test',
+        bootstrap_servers='kafka:9092',
         auto_offset_reset='earliest'
     )
     for message in consumer:
-        print(json.loads(message.value))
+        print(message.value.decode("utf-8"))
